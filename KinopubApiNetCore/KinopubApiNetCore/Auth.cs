99df60b455513f66d6502db0dd319508316d1534
Dmitry Tumashev <tumist@outlook.com>
2019-05-14 22:40:43 +0700

    Created API Library

---
+++ KinopubApiNetCore/KinopubApiNetCore/Auth.cs
@@ -0,0 +1,28 @@
+﻿using KinopubApiNetCore.Settings;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace KinopubApiNetCore.Auth
+{
+    class Auth
+    {
+        public static IRestResponse<DeviceCodeRequest> GetDeviceCodeAsync(string deviceId, string clientSecret)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var client = new RestClient(Constants.Domain);
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("oauth2/device", Method.POST);
+            request.AddParameter("grant_type", "device_code"); //Выбираем тип последующей авторизации - код устройства
+
+
+            // async with deserialization
+            var restResponse = client.ExecutePostTaskAsync<DeviceCodeRequest>(request, cancellationTokenSource.Token);
+            return restResponse.Result;
+        }
+
+    }
+}
---
+++ KinopubApiNetCore/KinopubApiNetCore/Settings/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KinopubApiNetCore.Settings
+{
+    public static class Constants
+    {
+        public static string Domain = "https://api.service-kp.com";
+    }
+}

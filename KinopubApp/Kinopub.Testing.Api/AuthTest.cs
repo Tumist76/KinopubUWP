efe4721f353585a39f0cfb595d6bfbd4ee9ce29d
Dmitry Tumashev <tuma98@gmail.com>
2019-05-20 00:13:41 +0700

    Изменил методологию тест авторизации

--- KinopubApp/Kinopub.Testing.Api/AuthTest.cs
+++ KinopubApp/Kinopub.Testing.Api/AuthTest.cs
@@ -12,12 +12,13 @@ namespace ApiTest
         public void ConneсtAndGetDeviceCode()
         {
             var response = Auth.GetDeviceCodeAsync("xbmc", "cgg3gtifu46urtfp2zp1nqtba0k2ezxh");
-            var statusCode = 200;
+
+            var expectedLength = 5;
 
             Debug.WriteLine("Device code: " + response.Result.Data.code);
             Debug.WriteLine("User code: " + response.Result.Data.user_code);
 
-            Assert.AreEqual(statusCode, (int)response.Result.StatusCode);
+            Assert.AreEqual(expectedLength, response.Result.Data.user_code.Length);
         }
     }
 }

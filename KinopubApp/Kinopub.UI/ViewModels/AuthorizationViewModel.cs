a67e1d92a9c8245fe19a8fadfee46e3099251a90
Dmitry Tumashev <tuma98@gmail.com>
2019-05-20 14:57:45 +0700

    Плохой нерабочий коммит

--- KinopubApp/Kinopub.UI/ViewModels/AuthorizationViewModel.cs
+++ KinopubApp/Kinopub.UI/ViewModels/AuthorizationViewModel.cs
@@ -15,6 +15,7 @@ namespace Kinopub.UI.ViewModels
         public AuthorizationViewModel()
         {
             GetDeviceCode();
+            UserCode = "*****";
         }
 
         #endregion
@@ -24,9 +25,19 @@ namespace Kinopub.UI.ViewModels
         /// <summary>
         /// Таск с информацией о коде устройства
         /// </summary>
-        public NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>> DeviceCodeRequest { get; set; }
+        public NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>> DeviceCodeRequest {
+            get => deviceCodeRequest;
+                set
+            {
+                deviceCodeRequest = value;
+                UserCode = value.Result.Data.user_code;
+            }
+        }
 
+        public string UserCode { get; set; }
         #endregion
+
+        private NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>> deviceCodeRequest;
         /// <summary>
         /// Вызывает таск получения кода устройства
         /// </summary>

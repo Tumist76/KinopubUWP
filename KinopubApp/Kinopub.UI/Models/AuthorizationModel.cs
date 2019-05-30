34c2a463a2085ca899eecb4ee629b2af55006447
Dmitry Tumashev <tumist@outlook.com>
2019-05-30 14:08:00 +0700

    Рефакторил авторизацию, вынес в модель

--- KinopubApp/Kinopub.UI/Models/AuthorizationModel.cs
+++ KinopubApp/Kinopub.UI/Models/AuthorizationModel.cs
@@ -1,19 +1,98 @@
 ﻿using Kinopub.Api;
+using Kinopub.Api.Entities.Auth;
 using Kinopub.UI.Utilities;
+using RestSharp.Portable;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.UI.Xaml;
 
 namespace Kinopub.UI.Models
 {
     /// <summary>
     /// Методы для проверки и получения токенов авторизации
     /// </summary>
-    public class AuthorizationModel
+    public class AuthorizationModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Публичные поля
+
+        /// <summary>
+        /// Обратный счётчик до окончания действия кода авторизации
+        /// </summary>
+        public int CountdownCounter { get; set; }
+        /// <summary>
+        /// Запрос на код устройства
+        /// </summary>
+        public IRestResponse<DeviceCodeRequest> DeviceCodeRequest { get; set; }
+
+        #endregion
+
+
+        #region Приватные поля
+
+        private DispatcherTimer countdownTimer;
+
+        #endregion
+
+
+        #region Методы
+
+        #region Получение токена авторизации
+
+        /// <summary>
+        /// Вызывает таск получения кода устройства
+        /// </summary>
+        public async Task GetDeviceCodeAsync()
+        {
+            DeviceCodeRequest = await Auth.GetDeviceCodeAsync(Constants.DeviceId, Constants.DeviceSecret);
+            if (DeviceCodeRequest.IsSuccess)
+            {
+                ExpirationCountdown();
+            }
+        }
+
+        /// <summary>
+        /// Выполняет запрос к серверу на получение токена авторизации и сохраняет при успехе
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetAccessTokenAsync()
+        {
+            var tokenRequest = await Auth.GetAccessTokenAsync(Constants.DeviceId, Constants.DeviceSecret, DeviceCodeRequest.Data.code);
+
+            if (tokenRequest.IsSuccess)
+            {
+                countdownTimer.Stop();
+                SaveAuthData
+                (tokenRequest.Data.access_token,
+                    tokenRequest.Data.refresh_token,
+                    tokenRequest.Data.expires_in
+                );
+                WindowNavigation.WindowNavigateTo(typeof(MainPage), null);
+            }
+        }
+
+        /// <summary>
+        /// Запускает таймер обратного отсчёта до прекращения действия кода авторизации
+        /// </summary>
+        private void ExpirationCountdown()
+        {
+            CountdownCounter = DeviceCodeRequest.Data.expires_in;
+            countdownTimer = new DispatcherTimer();
+            countdownTimer.Tick += CountdownTimer_Tick;
+            countdownTimer.Interval = new TimeSpan(0, 0, 1);
+            countdownTimer.Start();
+        }
+        
+        #endregion
+
+        #region Сохранение и обновление токена
+
         /// <summary>
         /// Возвращает композитное значение сохраненных данных авторизации
         /// </summary>
@@ -128,5 +207,29 @@ namespace Kinopub.UI.Models
 
             SettingsManager.SetLocalCompositeContainer(SettingsConstants.AuthData, composite);
         }
+        #endregion
+
+        #endregion
+
+        #region События
+
+        private void CountdownTimer_Tick(object sender, object e)
+        {
+            CountdownCounter--;
+            //Обновляет код по окончанию его действия
+            if (CountdownCounter <= 0)
+            {
+                countdownTimer.Stop();
+                GetDeviceCodeAsync();
+            }
+            //Выполняет запрос на получение токена доступа через заданный API интервал
+            if (CountdownCounter % DeviceCodeRequest.Data.interval == 0)
+            {
+                GetAccessTokenAsync();
+                return;
+            }
+        }
+
+        #endregion
     }
 }
--- KinopubApp/Kinopub.UI/ViewModels/AuthorizationViewModel.cs
+++ KinopubApp/Kinopub.UI/ViewModels/AuthorizationViewModel.cs
@@ -17,149 +17,45 @@ namespace Kinopub.UI.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private AuthorizationModel authorizationModel;
+
         #region Конструктор
 
         public AuthorizationViewModel()
         {
-            CodeRequest = new DeviceCodeRequest();
-
-            CodeRequest.user_code = "*****";
-
-            GetDeviceCode();
-
+            authorizationModel = new AuthorizationModel();
+            authorizationModel.PropertyChanged += AuthorizationViewModel_PropertyChanged;
+            authorizationModel.GetDeviceCodeAsync();
         }
 
         #endregion
+
 
         #region Публичные поля
 
-        /// <summary>
-        /// Результат запроса на код авторизации
-        /// </summary>
-        public DeviceCodeRequest CodeRequest { get; set; }
+        public IRestResponse<DeviceCodeRequest> DeviceCodeRequest { get; set; }
 
-        /// <summary>
-        /// Обратный счётчик до окончания действия кода авторизации
-        /// </summary>
         public int CountdownCounter { get; set; }
 
-        public NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>> DeviceCodeRequestTask { get; set; }
-
-        public NotifyTaskCompletion<IRestResponse<AccessTokenRequest>> AccessTokenRequestTask { get; set; }
-
         #endregion
 
-        #region Приватные поля
-
-        /// <summary>
-        /// Таск с информацией о коде устройства
-        /// </summary>
-        public NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>> deviceCodeRequestTask;
-
-        public NotifyTaskCompletion<IRestResponse<AccessTokenRequest>> accessTokenRequestTask;
-
-        private DispatcherTimer countdownTimer;
-
-        #endregion
-
-        #region Методы
-
-        /// <summary>
-        /// Вызывает таск получения кода устройства
-        /// </summary>
-        public void GetDeviceCode()
-        {
-            if (deviceCodeRequestTask != null) this.deviceCodeRequestTask.PropertyChanged -= AuthorizationViewModel_PropertyChanged;
-            deviceCodeRequestTask = new NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>>(Auth.GetDeviceCodeAsync(Constants.DeviceId, Constants.DeviceSecret));
-            //Подписываемся на обновление свойств внутри объекта с свойствами таска получения кода авторизации
-            this.deviceCodeRequestTask.PropertyChanged += AuthorizationViewModel_PropertyChanged;
-        }
-
-        public void GetAccessToken()
-        {
-            if (accessTokenRequestTask != null) this.accessTokenRequestTask.PropertyChanged -= AuthorizationViewModel_PropertyChanged;
-            accessTokenRequestTask = new NotifyTaskCompletion<IRestResponse<AccessTokenRequest>>(Auth.GetAccessTokenAsync(Constants.DeviceId, Constants.DeviceSecret, CodeRequest.code));
-            this.accessTokenRequestTask.PropertyChanged += AuthorizationViewModel_PropertyChanged;
-            
-        }
-
-        /// <summary>
-        /// Запускает таймер обратного отсчёта до прекращения действия кода авторизации
-        /// </summary>
-        private void ExpirationCountdown()
-        {
-            CountdownCounter = CodeRequest.expires_in;
-            countdownTimer = new DispatcherTimer();
-            countdownTimer.Tick += CountdownTimer_Tick;
-            countdownTimer.Interval = new TimeSpan(0, 0, 1);
-            countdownTimer.Start();
-        }
-        /// <summary>
-        /// Сохраняет данные авторизации и перенаправляет на главную страницу
-        /// </summary>
-        private void FinishAuthoriztion()
-        {
-            countdownTimer.Stop();
-            var authModel = new AuthorizationModel();
-            authModel.SaveAuthData
-                (AccessTokenRequestTask.Result.Data.access_token,
-                AccessTokenRequestTask.Result.Data.refresh_token,
-                AccessTokenRequestTask.Result.Data.expires_in
-                );
-            WindowNavigation.WindowNavigateTo(typeof(MainPage), null);
-        }
-
-
-        #endregion
 
         #region События
 
         void AuthorizationViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (sender.GetType() == typeof(NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>>))
+            switch (e.PropertyName)
             {
-                DeviceCodeRequestTask = sender as NotifyTaskCompletion<IRestResponse<DeviceCodeRequest>>;
-                switch (e.PropertyName)
-                {
-                    case "Result":
-                        if (deviceCodeRequestTask.IsSuccessfullyCompleted) CodeRequest = deviceCodeRequestTask.Result.Data;
-                        ExpirationCountdown();
-                        break;
-                }
-            }
-            if (sender.GetType() == typeof(NotifyTaskCompletion<IRestResponse<AccessTokenRequest>>))
-            {
-                AccessTokenRequestTask = sender as NotifyTaskCompletion<IRestResponse<AccessTokenRequest>>;
-                switch (e.PropertyName)
-                {
-                    case "Result":
-                        if (!String.IsNullOrEmpty(accessTokenRequestTask.Result.Data.access_token))
-                        {
-                            FinishAuthoriztion();
-                            break;
-                        }
-                        break;
-                }
-            }
-        }
-
-        private void CountdownTimer_Tick(object sender, object e)
-        {
-            CountdownCounter--;
-            //Обновляет код по окончанию его действия
-            if (CountdownCounter <= 0)
-            {
-                countdownTimer.Stop();
-                GetDeviceCode();
-            }
-            //Выполняет запрос на получение токена доступа через заданный API интервал
-            if (CountdownCounter % CodeRequest.interval == 0)
-            {
-                GetAccessToken();
-                return;
+                case "DeviceCodeRequest":
+                    DeviceCodeRequest = ((AuthorizationModel)sender).DeviceCodeRequest;
+                    break;
+                case "CountdownCounter":
+                    CountdownCounter = ((AuthorizationModel)sender).CountdownCounter;
+                    break;
             }
         }
 
         #endregion
+
     }
 }

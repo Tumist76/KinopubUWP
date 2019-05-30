3676e38dac7734b84921c13ca104f1a98edd3b03
Dmitry Tumashev <tumist@outlook.com>
2019-05-30 17:31:23 +0700

    Добавил хэндлер ошибок

---
+++ KinopubApp/Kinopub.Api/Entities/Error.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinopub.Api.Entities
+{
+    public class Error
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("code")]
+        public int Code { get; set; }
+        [JsonProperty("status")]
+        public int Status { get; set; }
+    }
+}
---
+++ KinopubApp/Kinopub.Api/ErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kinopub.Api.Entities;
+using Newtonsoft.Json;
+using RestSharp.Portable;
+
+namespace Kinopub.Api
+{
+    public static class ErrorHandler
+    {
+        public static Error GetErrorAsResult(IRestResponse response)
+        {
+            var error = JsonConvert.DeserializeObject<Error>(response.Content);
+            return error;
+        }
+    }
+}

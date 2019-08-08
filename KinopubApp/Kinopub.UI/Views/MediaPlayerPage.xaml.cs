41e179a8941e9dd7b56031fd017469c57cd18d38
Dmitry Tumashev <tumist@outlook.com>
2019-08-08 12:05:47 +0700

    Вставил инициализацию адаптивного потока в code-behind

--- KinopubApp/Kinopub.UI/Views/MediaPlayerPage.xaml.cs
+++ KinopubApp/Kinopub.UI/Views/MediaPlayerPage.xaml.cs
@@ -13,6 +13,11 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Kinopub.UI.ViewModels;
+using Windows.Media.Streaming.Adaptive;
+using Windows.Media.Core;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Media.Playback;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,6 +28,7 @@ namespace Kinopub.UI.Views
     /// </summary>
     public sealed partial class MediaPlayerPage : Page
     {
+        private AdaptiveMediaSource ams;
         public MediaPlayerPage()
         {
             this.InitializeComponent();
@@ -30,12 +36,47 @@ namespace Kinopub.UI.Views
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             var parameter = (string)e.Parameter;
-            ((MediaPlayerVM)DataContext).StreamUrl = parameter;
+            //((MediaPlayerVM)DataContext).StreamUrl = parameter;
+            InitializeAdaptiveMediaSource(new Uri(parameter));
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
 
+        }
+        async private void InitializeAdaptiveMediaSource(System.Uri uri)
+        {
+            AdaptiveMediaSourceCreationResult result = await AdaptiveMediaSource.CreateFromUriAsync(uri);
+
+            if (result.Status == AdaptiveMediaSourceCreationStatus.Success)
+            {
+                ams = result.MediaSource;
+                PlayerElement.SetMediaPlayer(new MediaPlayer());
+                PlayerElement.MediaPlayer.Source = MediaSource.CreateFromAdaptiveMediaSource(ams);
+                PlayerElement.MediaPlayer.Play();
+
+
+                //ams.InitialBitrate = ams.AvailableBitrates.Max<uint>();
+
+                ////Register for download requests
+                //ams.DownloadRequested += DownloadRequested;
+
+                ////Register for download failure and completion events
+                //ams.DownloadCompleted += DownloadCompleted;
+                //ams.DownloadFailed += DownloadFailed;
+
+                ////Register for bitrate change events
+                //ams.DownloadBitrateChanged += DownloadBitrateChanged;
+                //ams.PlaybackBitrateChanged += PlaybackBitrateChanged;
+
+                ////Register for diagnostic event
+                //ams.Diagnostics.DiagnosticAvailable += DiagnosticAvailable;
+            }
+            else
+            {
+                // Handle failure to create the adaptive media source
+                Debug.WriteLine($"Adaptive source creation failed: {uri} - {result.ExtendedError}");
+            }
         }
     }
 }

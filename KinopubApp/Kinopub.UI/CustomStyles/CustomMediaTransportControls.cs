6ad1cac06bd4a4e4fda98fff77feba3cde873984
Dmitry Tumashev <tumist@outlook.com>
2019-08-08 15:10:48 +0700

    Нерабочая кастомная панель воспроизведения

---
+++ KinopubApp/Kinopub.UI/CustomStyles/CustomMediaTransportControls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Kinopub.UI.CustomStyles
+{
+    class CustomMediaTransportControls : MediaTransportControls
+    {
+        public CustomMediaTransportControls()
+        {
+            this.DefaultStyleKey = typeof(CustomMediaTransportControls);
+        }
+
+
+    }
+}

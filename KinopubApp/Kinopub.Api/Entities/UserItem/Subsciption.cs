4e4de78151fda6f34c8b129ea5bc08f7544a4490
Dmitry Tumashev <tumist@outlook.com>
2019-10-01 17:30:59 +0700

    Начал создавать сущности пользователя

---
+++ KinopubApp/Kinopub.Api/Entities/UserItem/Subsciption.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Kinopub.Api.Entities.UserItem
+{
+    public class Subsciption
+    {
+        [JsonProperty("active")]
+        public bool Username { get; set; }
+
+        [JsonProperty("reg_date")] // 'Название / Оригинальное название'
+        public long RegistrationDate { get; set; }
+    }
+}
---
+++ KinopubApp/Kinopub.Api/Entities/UserItem/User.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Kinopub.Api.Entities.UserItem
+{
+    public class User
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("reg_date")] // 'Название / Оригинальное название'
+        public long RegistrationDate { get; set; }
+    }
+}
